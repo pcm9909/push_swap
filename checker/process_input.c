6b8599819f36dc59e58a347ac7a1722fa5df6d7d
pcm9909 <pcm9909@naver.com>
2024-05-17 00:52:00 +0900

    refactor norm

--- checker/process_input.c
+++ checker/process_input.c
@@ -1,49 +1,49 @@
 #include "./includes/checker.h"
 
-void execute_command(char *str, t_stack *a, t_stack *b)
+void	execute_command(char *str, t_stack *a, t_stack *b)
 {
-    if (ft_strcmp(str, "sa\n") == 0)
-        sa(a);
-    else if (ft_strcmp(str, "sb\n") == 0)
-        sb(b);
-    else if (ft_strcmp(str, "ss\n") == 0)
-        ss(a, b);
-    else if (ft_strcmp(str, "pa\n") == 0)
-        pa(a, b);
-    else if (ft_strcmp(str, "pb\n") == 0)
-        pb(a, b);
-    else if (ft_strcmp(str, "ra\n") == 0)
-        ra(a);
-    else if (ft_strcmp(str, "rb\n") == 0)
-        rb(b);
-    else if (ft_strcmp(str, "rr\n") == 0)
-        rr(a, b);
-    else if (ft_strcmp(str, "rra\n") == 0)
-        rra(a);
-    else if (ft_strcmp(str, "rrb\n") == 0)
-        rrb(b);
-    else if (ft_strcmp(str, "rrr\n") == 0)
-        rrr(a, b);
-    else
-        exit_with_err();
+	if (ft_strcmp(str, "sa\n") == 0)
+		sa(a);
+	else if (ft_strcmp(str, "sb\n") == 0)
+		sb(b);
+	else if (ft_strcmp(str, "ss\n") == 0)
+		ss(a, b);
+	else if (ft_strcmp(str, "pa\n") == 0)
+		pa(a, b);
+	else if (ft_strcmp(str, "pb\n") == 0)
+		pb(a, b);
+	else if (ft_strcmp(str, "ra\n") == 0)
+		ra(a);
+	else if (ft_strcmp(str, "rb\n") == 0)
+		rb(b);
+	else if (ft_strcmp(str, "rr\n") == 0)
+		rr(a, b);
+	else if (ft_strcmp(str, "rra\n") == 0)
+		rra(a);
+	else if (ft_strcmp(str, "rrb\n") == 0)
+		rrb(b);
+	else if (ft_strcmp(str, "rrr\n") == 0)
+		rrr(a, b);
+	else
+		exit_with_err();
 }
 
-void process_command(t_stack *a, t_stack *b, int size)
+void	process_command(t_stack *a, t_stack *b, int size)
 {
-    char *str;
+	char	*str;
 
-    while (1)
-    {
-        str = get_next_line(0);
-        if (!str)
-        {
-            if (is_sorted(a, size) == 1)
-                ft_putstr_fd("OK\n", 1);
-            else
-                ft_putstr_fd("KO\n", 1);
-            return;
-        }
-        execute_command(str, a, b);
-        free(str);
-    }
+	while (1)
+	{
+		str = get_next_line(0);
+		if (!str)
+		{
+			if (is_sorted(a, size) == 1)
+				ft_putstr_fd("OK\n", 1);
+			else
+				ft_putstr_fd("KO\n", 1);
+			return ;
+		}
+		execute_command(str, a, b);
+		free(str);
+	}
 }
